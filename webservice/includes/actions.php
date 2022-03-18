f53049bde4fb25de3315b6e3b534f240c0c9e2f6
PimMiii <pim.milt@gmail.com>
2022-03-18 19:07:07 +0100

    CSS card internal grid system

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -29,6 +29,26 @@ function getGames()
             "id" => 5,
             "name" => "Cities: Skylines",
             "developer" => "Colossal Order Ltd.",
+        ],
+        [
+            "id" => 6,
+            "name" => "Euro Truck Simulator 2",
+            "developer" => "SCS Software",
+        ],
+        [
+            "id" => 7,
+            "name" => "Monster Hunter: World",
+            "developer" => "CAPCOM Co., Ltd.",
+        ],
+        [
+            "id" => 8,
+            "name" => "Path of Exile",
+            "developer" => "Grinding Gear Games",
+        ],
+        [
+            "id" => 9,
+            "name" => "Tabletop Simulator",
+            "developer" => "Berserk Games",
         ]
     ];
 }
@@ -51,7 +71,7 @@ Winner or nominee of more than 150 “Game of the Year” awards, Rocket League 
             "playtime" => 395,
             "description" => "When a young street hustler, a retired bank robber and a terrifying psychopath find themselves entangled with some of the most frightening and deranged elements of the criminal underworld, the U.S. government and the entertainment industry, they must pull off a series of dangerous heists to survive in a ruthless city in which they can trust nobody, least of all each other.
 ",
-            "genres" => ['adventure', 'adventure']
+            "genres" => ['action', 'adventure']
         ],
         3 => [
             "playtime" => 287,
@@ -68,6 +88,26 @@ Winner or nominee of more than 150 “Game of the Year” awards, Rocket League 
             "description" => "Cities: Skylines is a modern take on the classic city simulation. The game introduces new game play elements to realize the thrill and hardships of creating and maintaining a real city whilst expanding on some well-established tropes of the city building experience. You’re only limited by your imagination, so take control and reach for the sky!",
             "genres" => ['simulation', 'strategy']
         ],
+        6 => [
+            "playtime" => 186,
+            "description" => "Travel across Europe as king of the road, a trucker who delivers important cargo across impressive distances! With dozens of cities to explore from the UK, Belgium, Germany, Italy, the Netherlands, Poland, and many more, your endurance, skill and speed will all be pushed to their limits. If you’ve got what it takes to be part of an elite trucking force, get behind the wheel and prove it!",
+            "genres" => ['indie', 'simulation']
+        ],
+        7 => [
+            "playtime" => 170,
+            "description" => "Welcome to a new world! Take on the role of a hunter and slay ferocious monsters in a living, breathing ecosystem where you can use the landscape and its diverse inhabitants to get the upper hand. Hunt alone or in co-op with up to three other players, and use materials collected from fallen foes to craft new gear and take on even bigger, badder beasts!",
+            "genres" => ['action']
+        ],
+        8 => [
+            "playtime" => 155,
+            "description" => "You are an Exile, struggling to survive on the dark continent of Wraeclast, as you fight to earn power that will allow you to exact your revenge against those who wronged you. Created by hardcore gamers, Path of Exile is an online Action RPG set in a dark fantasy world. With a focus on visceral action combat, powerful items and deep character customization, Path of Exile is completely free and will never be pay-to-win.",
+            "genres" => ['action', 'adventure', 'free to play', 'indie', 'massively multiplayer', 'rpg']
+        ],
+        9 => [
+            "playtime" => 154,
+            "description" => "Create your own original games, import custom assets, automate games with scripting, set up complete RPG dungeons, manipulate the physics, create hinges & joints, and of course flip the table when you are losing the game. All with an easy to use system integrated with Steam Workshop. You can do anything you want in Tabletop Simulator. The possibilities are endless!",
+            "genres" => ['casual', 'indie', 'rpg', 'simulation', 'strategy']
+        ]
     ];
 
     return $tags[$id];
