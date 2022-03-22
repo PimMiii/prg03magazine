6aea09d0a810dbf334a8ac231be973493a6a4bea
PimMiii <pim.milt@gmail.com>
2022-03-22 12:54:46 +0100

    API call added Card building added

--- js/main.js
+++ js/main.js
@@ -2,8 +2,55 @@ window.addEventListener('load', init)
 
 
 // GLOBALS
-content = document.getElementById('content')
+let apiUrl = "http://localhost/magazine/webservice";
+let content = document.getElementById('content');
 
 function init() {
-    console.log(content)
+    getGameData(apiUrl, createGameCards)
+
+}
+
+function getGameData(url, succcessHandler) {
+    fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(succcessHandler)
+        .catch(ajaxErrorHandler);
+}
+
+function createGameCards(data) {
+    for (let game of data){
+        // wrapper container for the game info
+        let gameCard = document.createElement('div');
+        gameCard.classList.add('card');
+        gameCard.dataset.id = game.id;
+        // game name info
+        let gameName = document.createElement('h2');
+        gameName.innerHTML = game.name;
+        gameCard.appendChild(gameName);
+
+        // add a favourite and info button
+        let favorite = document.createElement('button');
+        favorite.classList.add('add-to-favorites');
+        favorite.innerHTML = '<i class="fa-regular fa-heart"></i>'
+        gameCard.appendChild(favorite);
+        let info = document.createElement('button');
+        info.classList.add('show-description')
+        info.innerHTML = '<i class="fa-solid fa-circle-info"></i>'
+        gameCard.appendChild(info);
+
+        content.appendChild(gameCard);
+    }
+}
+
+function ajaxErrorHandler(data) {
+    console.log(data)
+    let error = document.createElement('div');
+    error.classList.add('error');
+    error.innerHTML = "Er ging iets fout. Probeer later nog eens!";
+    content.before(error);
 }
