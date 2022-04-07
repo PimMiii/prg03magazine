16774454fd66d2539e2607e0b3aab9e51df4289d
PimMiii <pim.milt@gmail.com>
2022-04-07 13:10:32 +0200

    general clean-up of main.js

--- js/main.js
+++ js/main.js
@@ -4,28 +4,28 @@ window.addEventListener('load', init)
 // GLOBALS
 const apiUrl = "http://localhost/magazine/webservice";
 let content = document.getElementById('content');
-const secretsUrl = "./js/secrets.json"
-let secrets;
 let description;
 let favorites = [];
 let gamesList = [];
+let clickedItem;
+let previousTarget;
+
 // check if favorites has been set in localstorage
 if (localStorage.getItem("favorites") !== null) {
     //if favorites has been set in localstorage
     favorites = JSON.parse(localStorage.getItem("favorites"));
 }
-let clickedItem;
-let previousTarget;
 
 
 function init() {
-    // create gamecards
-    fetchData(apiUrl, createGameCards)
-    content.addEventListener('click', contentClickHandler)
+    //create game-cards
+    fetchData(apiUrl, createGameCards);
+    //add click events to the content
+    content.addEventListener('click', contentClickHandler);
 }
 
 // function to fetch data from APIs
-function fetchData(url, succcessHandler) {
+function fetchData(url, successHandler) {
     fetch(url)
         .then((response) => {
             if (!response.ok) {
@@ -33,7 +33,7 @@ function fetchData(url, succcessHandler) {
             }
             return response.json();
         })
-        .then(succcessHandler)
+        .then(successHandler)
         .catch(ajaxErrorHandler);
 }
 
@@ -45,7 +45,7 @@ function createGameCards(data) {
         let gameCard = document.createElement('div');
         gameCard.classList.add('card');
         gameCard.dataset.appid = game.appid;
-        gameCard.appid = game.appid
+        gameCard.appid = game.appid;
         // game name info
         let gameName = document.createElement('h2');
         gameName.innerHTML = game.name;
@@ -53,8 +53,8 @@ function createGameCards(data) {
 
         // add cover image to the card
         let cover = document.createElement('img');
-        // headers come from a different link than the steamwebapi uses
-        cover.src = `https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/header.jpg`
+        // headers come from a different link than the SteamWebAPI uses
+        cover.src = `https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/header.jpg`;
         gameCard.appendChild(cover);
 
         // add a favourite and info button
@@ -63,8 +63,8 @@ function createGameCards(data) {
         favorite.dataset.appid = game.appid;
         // set the icon to be the outline variant
         let favIcon = document.createElement('i');
-        favIcon.className = 'fa-regular fa-heart'
-        favorite.appendChild(favIcon)
+        favIcon.className = 'fa-regular fa-heart';
+        favorite.appendChild(favIcon);
         // if the id is in favorites, set the icon to be the filled variant
         for (let i in favorites) {
             if (favorites[i] === favorite.dataset.appid) {
@@ -77,8 +77,8 @@ function createGameCards(data) {
         info.classList.add('show-description');
         info.dataset.appid = game.appid;
         let infoIcon = document.createElement('i');
-        infoIcon.className = 'fa-solid fa-circle-info'
-        info.appendChild(infoIcon)
+        infoIcon.className = 'fa-solid fa-circle-info';
+        info.appendChild(infoIcon);
         gameCard.appendChild(info);
 
         // add the created card to the contents of the webpage
@@ -90,10 +90,10 @@ function createGameCards(data) {
 function descriptionBuilder(data) {
     let information = data;
     // find the right game, and grab the playtime
-    let appid = information['appid']
+    let appid = information['appid'];
     for (let game of gamesList) {
         if (game['appid'] === appid) {
-            information.icon = game['img_icon_url']
+            information.icon = game['img_icon_url'];
             information.playtime = Math.floor(game['playtime_forever'] / 60);
         }
     }
@@ -101,9 +101,9 @@ function descriptionBuilder(data) {
     // if there is no description yet
     if (description === undefined) {
         // create the div and assign card class and description id
-        description = document.createElement('div')
-        description.classList.add('card')
-        description.id = 'description'
+        description = document.createElement('div');
+        description.classList.add('card');
+        description.id = 'description';
 
     } else {
         // when a description is already active, select it and empty it's innerHTML
@@ -118,11 +118,11 @@ function descriptionBuilder(data) {
         keyElement.classList.add(key);
         switch (key) {
             case 'playtime':
-                keyElement.innerHTML = `${information[key]} uur in-game`
+                keyElement.innerHTML = `${information[key]} uur in-game`;
                 break;
             case 'genres':
                 let genres = [];
-                for (genre of information[key]) {
+                for (let genre of information[key]) {
                     genres.push(genre['description']);
                 }
                 keyElement.innerHTML = genres.join(', ');
@@ -134,11 +134,11 @@ function descriptionBuilder(data) {
                 keyElement.appendChild(link);
                 break;
             case 'appid':
-                keyElement.innerHTML = `App ID: ${information[key]}`
+                keyElement.innerHTML = `App ID: ${information[key]}`;
                 break;
             case 'icon':
                 let icon = document.createElement('img');
-                let url = `http://media.steampowered.com/steamcommunity/public/images/apps/${information['appid']}/${information[key]}.jpg`;
+                let url = `https://media.steampowered.com/steamcommunity/public/images/apps/${information['appid']}/${information[key]}.jpg`;
                 icon.src = url;
                 keyElement.appendChild(icon);
                 break;
@@ -162,51 +162,51 @@ function contentClickHandler(e) {
         switch (parentItem.className) {
             case 'add-to-favorites':
                 // favorite icon has been clicked
-                favoriteClickHandler(clickedItem, parentItem)
+                favoriteClickHandler(clickedItem, parentItem);
                 break;
             case 'show-description':
                 // info icon has been clicked
-                infoClickHandler(clickedItem, parentItem)
+                infoClickHandler(clickedItem, parentItem);
                 break;
         }
     }
 }
 
-function favoriteClickHandler(clickedItem, parentItem, e) {
+function favoriteClickHandler(clickedItem, parentItem) {
     let found = false;
     // check if clicked item is already in favorites.
-    // if it is, delete from favorites => unfavorite item.
+    // if it is, delete from favorites => un-favorite item.
     for (let i in favorites) {
         if (favorites[i] === parentItem.dataset.appid) {
             found = true;
             favorites.splice(i, 1);
-            clickedItem.className = 'fa-regular fa-heart'
+            clickedItem.className = 'fa-regular fa-heart';
         }
     }
-    // if it's not already favorited, add it to favorites.
+    // if it's not already in favorites, add it to favorites.
     if (!found) {
         favorites.push(parentItem.dataset.appid);
-        clickedItem.className = 'fa-solid fa-heart'
+        clickedItem.className = 'fa-solid fa-heart';
     }
     // save favorites to localstorage
     localStorage.setItem('favorites', JSON.stringify(favorites))
 }
 
-function infoClickHandler(clickedItem, parentItem, e) {
+function infoClickHandler(clickedItem, parentItem) {
     // check to see if
     if (previousTarget !== undefined) {
         previousTarget.id = '';
     }
     clickedItem.id = 'description-active';
-    previousTarget = clickedItem
-    console.log(`${apiUrl}?id=${parentItem.dataset.appid}`)
-    fetchData(`${apiUrl}?id=${parentItem.dataset.appid}`, descriptionBuilder)
+    previousTarget = clickedItem;
+    console.log(`${apiUrl}?id=${parentItem.dataset.appid}`);
+    fetchData(`${apiUrl}?id=${parentItem.dataset.appid}`, descriptionBuilder);
 
 
 }
 
 function ajaxErrorHandler(data) {
-    console.log(data)
+    console.log(data);
     let error = document.createElement('div');
     error.classList.add('error');
     error.innerHTML = "Er ging iets fout. Probeer later nog eens!";
