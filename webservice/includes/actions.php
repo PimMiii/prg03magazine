d37bf01bb22cc1bfeba8c1e15ee0f8b616fd4005
PimMiii <pim.milt@gmail.com>
2022-04-07 13:50:50 +0200

    cleanup of actions.php

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -2,45 +2,46 @@
 require_once 'steamapi.php';
 $games = [];
 $lastUpdate = false;
-$timeout = 60 * 60; // set timeout to 60 minutes ( 60 times 60 seconds)
+$timeout = 60 * 60; // set timeout to 60 minutes ( 60 times 60 seconds).
 
 
-/**
- * @return array
- */
+
 function getGames()
 {
-    // fetch all games from my library
+    // fetch all games from my library.
     $data = getSteamGames();
-    // we only need the games
+    // we only need the games.
     $data = $data['response']['games'];
     // sort data array by playtime in descending order.
     array_multisort(array_column($data, 'playtime_forever'), SORT_DESC, $data);
     return $data;
 }
 
-/**
- * @param $id
- * @return mixed
- */
+
 function getGameDetails($id)
 {
-    // fetch game info from steamapi
+    // fetch game info from SteamWebAPI.
     $result = getSteamInfo($id);
-    // whitelist for the keys we want te retain
+    // whitelist for the keys we want te retain.
     $whitelist = ['steam_appid', 'name', 'short_description', 'website', 'developers', 'genres'];
     $result = $result[$id]['data'];
-    // empty array for output
+    // empty array for output.
     $output = [];
-    // loop through each key in the whitelist
+    // loop through each key in the whitelist.
     foreach ($whitelist as $key) {
-        // rename the steam_appid key to appid
-        if ($key === 'steam_appid') {
-            $output['appid'] = $result[$key];
-        }
-        // place other whitelisted keys into the output array
-        else {
-            $output[$key] = $result[$key];
+        switch ($key) {
+            // rename the steam_appid key to appid.
+            case  'steam_appid':
+                $output['appid'] = $result[$key];
+                break;
+            // only take the first developer if there are multiple listed.
+            case 'developers':
+                $output[$key] = $result[$key][0];
+                break;
+            // place other whitelisted keys into the output array.
+            default:
+                $output[$key] = $result[$key];
+                break;
         }
     }
     return $output;
